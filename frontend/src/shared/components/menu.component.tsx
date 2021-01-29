594aa6b197dc9701b563fffe20fc244e08b61f86
Tam Le <philosocode@gmail.com>
2021-01-28 20:28:52 -0700

    Get menu actions working

--- frontend/src/shared/components/menu.component.tsx
+++ frontend/src/shared/components/menu.component.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect, useState } from "react";
+import React, { FC, MutableRefObject, useEffect, useRef, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { IconProp } from "@fortawesome/fontawesome-svg-core";
 import styled from "styled-components";
@@ -15,37 +15,50 @@ interface IProps {
   actions: IMenuAction[];
 }
 
+// Referenced: https://letsbuildui.dev/articles/building-a-dropdown-menu-component-with-react-hooks
 export const Menu: FC<IProps> = ({ actions }) => {
   const [menuShowing, setMenuShowing] = useState(false);
+  const menuRef = useRef() as MutableRefObject<HTMLDivElement>;
 
   useEffect(() => {
-    menuShowing
-      ? document.addEventListener("mousedown", hideMenu)
-      : document.removeEventListener("mousedown", hideMenu);
+    if (menuShowing) {
+      window.addEventListener("mousedown", hideMenu);
+    }
+
+    return () => {
+      window.removeEventListener("mousedown", hideMenu);
+    }
   }, [menuShowing]);
 
   const showMenu = () => setMenuShowing(true);
-  const hideMenu = () => setMenuShowing(false);
 
-  function handleActionClick(action: () => void) {
-    hideMenu();
+  const hideMenu = (event: React.MouseEvent | MouseEvent) => {
+    if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+      setMenuShowing(false);
+    }
+  }
+
+  function handleActionClick(event: React.MouseEvent, action: () => void) {
     action();
+    setMenuShowing(false);
   }
 
   return (
     <SContainer>
       <SIcon icon="ellipsis-v" onClick={showMenu} />
       {menuShowing ? (
-        <SActionList>
-          {
-            actions.map(action => (
-              <SAction onClick={() => handleActionClick(action.action)} key={action.name}>
-                <SActionIcon icon={action.icon} />
-                {action.name}
-              </SAction>
-            ))
-          }
-        </SActionList>
+        <div ref={menuRef}>
+         <SActionList>
+            {
+              actions.map(action => (
+                <SAction onClick={(event) => handleActionClick(event, action.action)} key={action.name}>
+                  <SActionIcon icon={action.icon} />
+                  {action.name}
+                </SAction>
+              ))
+            }
+          </SActionList>
+        </div>
       ) : null}
     </SContainer>
   );
@@ -67,8 +80,8 @@ const SActionList = styled.div`
   display: flex;
   flex-direction: column;
   position: absolute;
-  right: 1.5rem;
-  top: 0;
+    right: 1.5rem;
+    top: 0;
   width: max-content;
 `;
 
