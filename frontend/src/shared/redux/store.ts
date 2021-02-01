43f43f3a340c36efb995990db48b0d8790e4d414
Tam Le <philosocode@gmail.com>
2021-01-31 20:47:29 -0700

    Restructure reducer to clear state on logout

--- frontend/src/shared/redux/store.ts
+++ frontend/src/shared/redux/store.ts
@@ -1,12 +1,36 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { AnyAction, combineReducers, configureStore, Reducer } from "@reduxjs/toolkit";
 import logger from "redux-logger";
 
-import { rootReducer } from "./root-reducer";
+import { alertReducer } from "alert/redux/alert.slice";
+import { authReducer } from "auth/redux/auth.slice";
+import { conceptReducer } from "concept/redux/concept.slice";
+import { loadingReducer } from "loading/redux/loading.slice";
+import { modalReducer } from "modal/redux/modal.slice";
+import { userReducer } from "user/redux/user.slice";
+
+// // FROM: https://stackoverflow.com/a/35641992
+const appReducer = combineReducers({
+  alert: alertReducer,
+  auth: authReducer,
+  concept: conceptReducer,
+  loading: loadingReducer,
+  modal: modalReducer,
+  user: userReducer,
+});
+
+export type TAppState = ReturnType<typeof appReducer>;
+
+const rootReducer: Reducer = (state: TAppState, action: AnyAction) => {
+  if (action.type === "auth/logout") {
+    state = {} as TAppState;
+  }
+
+  return appReducer(state, action);
+}
 
 export const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
-export type TAppState = ReturnType<typeof rootReducer>;
 export type TAppDispatch = typeof store.dispatch;
