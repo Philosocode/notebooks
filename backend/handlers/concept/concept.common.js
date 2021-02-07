bb4cf3570112b3a8c18ee220a3aa9daa41ade45a
Tam Le <philosocode@gmail.com>
2021-02-06 20:21:26 -0700

    Change conceptExistsMiddleware to use generic entityExists helper

--- backend/handlers/concept/concept.common.js
+++ backend/handlers/concept/concept.common.js
@@ -1,13 +1,13 @@
 const AppError = require("../../utils/app-error.util");
 const catchAsync = require("../../middlewares/catch-async.middleware");
-const { conceptExists } = require("../../models/concept.model");
+const { entityExists } = require("../../models/common.model");
 
 // use on concept routes that take a :conceptId param
-exports.conceptExistsMiddleware = catchAsync(async function conceptExistsMiddleware(req, res, next) {
+exports.conceptExistsMiddleware = catchAsync(async function conceptExistsMiddleware(req, _, next) {
   const userId = req.user.id;
   const { conceptId } = req.params;
 
-  const exists = await conceptExists(userId, { id: conceptId });
+  const exists = await entityExists("concept", { id: conceptId, user_id: userId });
   if (!exists) {
     return next(new AppError("Concept not found.", 404));
   }
