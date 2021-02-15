c9d281c5a39529306b1cfec91d1a79d2beba39d0
Tam Le <philosocode@gmail.com>
2021-02-15 15:48:20 -0700

    Add concept detail header

--- frontend/src/concept/components/concept-detail-header.component.tsx
+++ frontend/src/concept/components/concept-detail-header.component.tsx
@@ -1,17 +1,38 @@
 import React, { FC } from "react";
+import styled from "styled-components";
+import format from "date-fns/format";
 
 import { IConcept } from "concept/redux/concept.types";
 import { SHeadingSubtitle } from "shared/styles/typography.style";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { theme } from "shared/styles/theme.style";
 
 interface IProps {
   concept: IConcept;
 }
 export const ConceptDetailHeader: FC<IProps> = ({ concept }) => {
+  const formattedUpdateDate = format(concept.updated_at, "PPP");
   return (
-    <div>
+    <SContainer>
+      <SSettingsIcon icon="cog" />
       <SHeadingSubtitle>{concept.name}</SHeadingSubtitle>
-      <div>Created: {concept.created_at.toUTCString()}</div>
-      <div>Last Updated: {concept.updated_at.toUTCString()}</div>
-    </div>
+      <div>Last Updated: {formattedUpdateDate}</div>
+    </SContainer>
   )
 }
+
+const SContainer = styled.div`
+  position: relative;
+`;
+
+const SSettingsIcon = styled(FontAwesomeIcon)`
+  cursor: pointer;
+  font-size: 2rem;
+  position: absolute;
+    left: -2.75rem;
+    top: 8px;
+
+  &:hover {
+    color: ${theme.colors.green[400]};
+  }
+`;
