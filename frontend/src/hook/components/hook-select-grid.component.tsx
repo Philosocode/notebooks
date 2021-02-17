2dea1689a051a42b7219f95d6cb2f12ecb587ecc
Tam Le <philosocode@gmail.com>
2021-02-16 19:52:36 -0700

    Add input to filter hooks

--- frontend/src/hook/components/hook-select-grid.component.tsx
+++ frontend/src/hook/components/hook-select-grid.component.tsx
@@ -1,30 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
+// styles
 import { theme } from "../../shared/styles/theme.style";
+import { SInputBorderless } from "shared/styles/form.style";
+import { SHeadingSubSubtitle } from "shared/styles/typography.style";
 
 interface IProps {
   handleSelect: (hook: string) => void;
   hooks: string[];
 }
 export const HookSelectGrid: React.FC<IProps> = ({ handleSelect, hooks }) => {
+  const [filterText, setFilterText] = useState("");
+
+  function getFilteredHooks() {
+    if (filterText.trim() === "") return hooks;
+
+    const filterTrimLower = filterText.trim().toLowerCase();
+
+    return hooks.filter((hook) =>
+      hook.toLowerCase().includes(filterTrimLower.toLowerCase())
+    );
+  }
+
+  function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setFilterText(event.target.value);
+  }
+
   return (
-    <SHookList>
-      {hooks.map((hook) => (
+    <div>
+      <SInput onChange={handleInputChange} />
+      <SHookGrid>
+        { getFilteredHooks().length === 0 && (
+          <SHeadingSubSubtitle>No hooks found.</SHeadingSubSubtitle>
+        )}
+        {getFilteredHooks().map((hook) => (
           <SHookCard key={hook} onClick={() => handleSelect(hook)}>
             {hook}
           </SHookCard>
         ))}
-    </SHookList>
+      </SHookGrid>
+    </div>
   );
 };
 
-const SHookList = styled.div`
+const SInput = styled(SInputBorderless)`
+  max-width: 40rem;
+  margin-top: ${theme.spacing.base};
+`;
+
+const SHookGrid = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(25rem, 1fr));
-  justify-items: space-between;
-  gap: ${theme.spacing.base};
-  margin-top: ${theme.spacing.sm};
+    grid-template-columns: repeat(auto-fill, minmax(25rem, 1fr));
+    justify-items: space-between;
+    gap: ${theme.spacing.base};
+  margin-top: ${theme.spacing.base};
 `;
 
 const SHookCard = styled.div`
