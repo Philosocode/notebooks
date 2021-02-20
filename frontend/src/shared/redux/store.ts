361da3bd6e1589de53bcec2b88f437ecacf847a1
Tam Le <philosocode@gmail.com>
2021-02-19 20:37:03 -0700

    Disable Redux serializable check for modal actions.
    This is done because functions are passed in modal props, but this
    causes the Redux middleware it to complain.

--- frontend/src/shared/redux/store.ts
+++ frontend/src/shared/redux/store.ts
@@ -1,10 +1,10 @@
-import { AnyAction, combineReducers, configureStore, Reducer } from "@reduxjs/toolkit";
+import { AnyAction, combineReducers, configureStore, getDefaultMiddleware, Reducer } from "@reduxjs/toolkit";
 
 import { alertReducer } from "alert/redux/alert.slice";
 import { authReducer } from "auth/redux/auth.slice";
 import { conceptReducer } from "concept/redux/concept.slice";
 import { loadingReducer } from "loading/redux/loading.slice";
-import { modalReducer } from "modal/redux/modal.slice";
+import { modalReducer, showModal, hideModal } from "modal/redux/modal.slice";
 import { userReducer } from "user/redux/user.slice";
 
 const appReducer = combineReducers({
@@ -30,7 +30,11 @@ const rootReducer: Reducer = (state: TAppState, action: AnyAction) => {
 
 export const store = configureStore({
   reducer: rootReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [showModal.type, hideModal.type]
+    }
+  })
 });
 
 export type TAppDispatch = typeof store.dispatch;
