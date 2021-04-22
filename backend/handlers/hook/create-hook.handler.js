fc9fe9540d9a50c9abd325b79f81e93c7bd12f11
Tam Le <philosocode@gmail.com>
2021-04-21 20:55:19 -0600

    Fix hook DND

--- backend/handlers/hook/create-hook.handler.js
+++ backend/handlers/hook/create-hook.handler.js
@@ -3,7 +3,7 @@ const sendResponse = require("../response.handler");
 const catchAsync = require("../../middlewares/catch-async.middleware");
 const { createHook } = require("../../models/hook.model");
 const { trimString } = require("../../utils/string.util");
-const { getValidInsertPosition } = require("./hook.common");
+const { getValidInsertPosition } = require("../../common/common.model");
 
 module.exports = catchAsync(async function (req, res, next) {
   const { conceptId } = req.params;
@@ -24,7 +24,11 @@ module.exports = catchAsync(async function (req, res, next) {
 
   // name has a max length of 100 chars
   const trimmedName = trimString(name, 100);
-  const insertPosition = await getValidInsertPosition(conceptId, position, true);
+  const insertPosition = await getValidInsertPosition(
+    "hook",
+    { "concept_id": conceptId },
+    position,
+    true);
   const createdHook = await createHook(conceptId, trimmedName, content, insertPosition);
 
   sendResponse(res, 201, {
